6798f9dffab7464422304da638343b643b9b22eb
Ivan Gerov <ivangerov93@gmail.com>
2024-02-05 18:46:20 +0200

    SOLID - Lab Exercises

---
+++ SOLID - Lab/06.SOLID/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using _06.SOLID.Promotions;
+using OpenClosePrinciple;
+
+Store store = new Store(new List<IPromotion>()
+            {
+                //new DecemberPromotion(),
+                new MondayPromotion(),
+                //new StValentinePromotion(),
+                new SundayPromotion(),
+                new HourPromotion()
+            });
+
+store.Products.Add(new Product()
+{
+    Name = "Teniska",
+    Price = 100,
+    Quantity = 60
+});
+
+store.Products.Add(new Product()
+{
+    Name = "Obuvki",
+    Price = 200,
+    Quantity = 60
+});
+
+
+store.Products.Add(new Product()
+{
+    Name = "4asovni4e",
+    Price = 500,
+    Quantity = 30
+});
+
+while (true)
+{
+    Console.WriteLine("What do you want to buy?");
+    string productName = Console.ReadLine();
+
+    store.Buy(productName);
+}
---
+++ SOLID - Lab/06.SOLID/Store.cs
@@ -0,0 +1,44 @@
+﻿using _06.SOLID.Promotions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosePrinciple
+{
+    public class Store
+    {
+        private List<IPromotion> promotions;
+
+        public Store(List<IPromotion> promotions)
+        {
+            Products = new List<Product>();
+
+            this.promotions = promotions;
+        }
+
+        public List<Product> Products { get; set; }
+
+        public void Buy(string productName)
+        {
+            Product product = Products.FirstOrDefault(p => p.Name == productName);
+            if (product == null)
+            {
+                throw new ArgumentException("Product not found");
+            }
+
+            // take money from customer
+            decimal price = product.Price;
+
+            foreach (var promotion in promotions)
+            {
+                price -= promotion.GetPrice(product);
+            }
+
+            Console.WriteLine($"Buing {productName} for {price}");
+
+            product.Quantity -= 1;
+        }
+    }
+}
